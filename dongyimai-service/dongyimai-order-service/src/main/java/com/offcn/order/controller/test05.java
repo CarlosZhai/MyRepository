44f74bfb4f174502325ac45f612326c93ace8ae9
Carlos <106510342+CarlosZhai@users.noreply.github.com>
2022-10-11 23:03:37 +0800

    Update test05.java

--- dongyimai-service/dongyimai-order-service/src/main/java/com/offcn/order/controller/test05.java
+++ dongyimai-service/dongyimai-order-service/src/main/java/com/offcn/order/controller/test05.java
@@ -3,5 +3,6 @@ package com.offcn.order.controller;
 public class test05 {
     public static void main(String[] args) {
         System.out.println("test05");
+        System.out.println("test055");
     }
 }
