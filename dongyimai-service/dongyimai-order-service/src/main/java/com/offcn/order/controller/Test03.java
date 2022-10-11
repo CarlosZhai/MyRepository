99b019bb7f2cdd3d5778242939fbc77a18007c44
Carlos <106510342+CarlosZhai@users.noreply.github.com>
2022-10-11 22:40:20 +0800

    Update Test03.java

--- dongyimai-service/dongyimai-order-service/src/main/java/com/offcn/order/controller/Test03.java
+++ dongyimai-service/dongyimai-order-service/src/main/java/com/offcn/order/controller/Test03.java
@@ -3,5 +3,6 @@ package com.offcn.order.controller;
 public class Test03 {
     public static void main(String[] args) {
         System.out.println("master1");
+        System.out.println("第一次修改master1");
     }
 }
