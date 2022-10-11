cb5b3f6b83cecd10b5c2f78f8e1306789c32216b
Carlos <106510342+CarlosZhai@users.noreply.github.com>
2022-10-11 22:36:28 +0800

    Update Test03.java

--- dongyimai-service/dongyimai-order-service/src/main/java/com/offcn/order/controller/Test03.java
+++ dongyimai-service/dongyimai-order-service/src/main/java/com/offcn/order/controller/Test03.java
@@ -2,11 +2,6 @@ package com.offcn.order.controller;
 
 public class Test03 {
     public static void main(String[] args) {
-        System.out.println("远程仓库。。。");
-        System.out.println("本地。。。");
-        
-        System.out.println("test222rrrr");
-        System.out.println("test333");
-
+        System.out.println("master1");
     }
 }
