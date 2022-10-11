c86a8fd9275809a4426b668ffd6b3a5f71d0994b
offcn-java <zhaijia421182@163.com>
2022-10-11 22:21:41 +0800

    mydev2

--- dongyimai-service/dongyimai-order-service/src/main/java/com/offcn/order/controller/Test03.java
+++ dongyimai-service/dongyimai-order-service/src/main/java/com/offcn/order/controller/Test03.java
@@ -4,5 +4,6 @@ public class Test03 {
     public static void main(String[] args) {
         System.out.println("远程仓库。。。");
         System.out.println("本地。。。");
+        System.out.println("DEV2");
     }
 }
